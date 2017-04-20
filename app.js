62d47808920429efd1dd556c392917c54d750388
miskamyasa <miskamyasa@Drakkar.lan>
2017-04-20 19:46:46 +0300

    some changes made

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+process.env.NODE_ENV !== 'production' && require('dotenv').config()
 var express = require('express')
 var path = require('path')
 var favicon = require('serve-favicon')
-var logger = require('morgan')
+var morgan = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 
@@ -10,17 +11,18 @@ var users = require('./routes/users')
 
 var app = express()
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'))
+  app.use(express.static(path.join(__dirname, 'public_html')))
+}
 
 app.use('/', index)
 app.use('/users', users)
